c7533919ee99f92018b58071311ce416577f3eab
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-12 15:52:19 +0200

    Commit, class "Partecipazione" created

--- src/main/java/gestioneEventi/Partecipazione.java
+++ src/main/java/gestioneEventi/Partecipazione.java
@@ -1,5 +1,54 @@
 package gestioneEventi;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "partecipazioni")
+
 public class Partecipazione {
 
+	// id (chiave primaria)
+	//
+	//
+	//
+	//
+	//
+
+	@Id
+	// @GeneratedValue
+	private long id;
+
+	// *** TODO *** persona;
+	// *** TODO *** evento;
+	private Stato stato;
+
+	// costruttori, getters e setters...
+	public Partecipazione() {
+
+	}
+
+	public Partecipazione(long _id, Stato _stato) {
+		this.id = _id;
+		this.stato = _stato;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Stato getStato() {
+		return stato;
+	}
+
+	public void setStato(Stato _stato) {
+		this.stato = _stato;
+	}
+
+	@Override
+	public String toString() {
+		return "Partecipazione [ " + id + ", " + stato + " ]";
+
+	}
 }
