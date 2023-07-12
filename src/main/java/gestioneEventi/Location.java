2d2637e6225db5710b1b14057ab478131f5fdc70
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-12 15:42:53 +0200

    Commit, class "Location" created

--- src/main/java/gestioneEventi/Location.java
+++ src/main/java/gestioneEventi/Location.java
@@ -1,5 +1,62 @@
 package gestioneEventi;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "locations")
+
 public class Location {
 
+	// id (chiave primaria)
+	//
+	//
+	//
+	//
+	//
+
+	@Id
+	// @GeneratedValue
+	private long id;
+
+	private String nome;
+	private String citta;
+
+	// costruttori, getters e setters...
+	public Location() {
+
+	}
+
+	public Location(long _id, String _nome, String _citta) {
+		this.id = _id;
+		this.nome = _nome;
+		this.citta = _citta;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String _nome) {
+		this.nome = _nome;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String _citta) {
+		this.citta = _citta;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [ " + id + ", " + nome + ", " + citta + " ]";
+
+	}
 }
